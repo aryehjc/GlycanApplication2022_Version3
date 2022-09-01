8f423aeaf93905d1011cc6d66bfa816ecbaf7442
Aryeh <83979895+aryehjc@users.noreply.github.com>
2022-09-01 17:05:17 +0800

    Update GlycanService.java

--- src/main/java/com/example/demo/service/GlycanService.java
+++ src/main/java/com/example/demo/service/GlycanService.java
@@ -15,46 +15,46 @@ public class GlycanService {
 	//	We will use it to handle the data
 	GlycanRepository glycanRepository;
 
-	//	get all employees
+	//	get all glycans
 	public List<Glycan> getAllGlycans() {
 
-		//		find all employees data, then return it
+		//		find all glycans data, then return it
 		return glycanRepository.findAll();
 	}
 
-	// save an employee
+	// save an glycan
 	public void saveGlycan(Glycan gly) {
             String image = gly.getPhotosImagePath();
             if (image != null) {
       		glycanRepository.save(gly);
             }
-		//		save employee data to database
+		//		save glycan data to database
 
 	}
 
-	//	get employee by id
+	//	get glycan by id
 	public Glycan getGlycanById(Long id) {
-		//		find an employee by id
+		//		find an glycan by id
 		Glycan gly = glycanRepository.getById(id);
 
-		//		if there is not an employee who has the id, throw the error.
+		//		if there is not an glycan who has the id, throw the error.
 		if (gly == null) {
 			throw new RuntimeException("Glycan not found");
 		}
 		return gly;
 	}
 
-	//	delete employee by id
+	//	delete glycan by id
 	public String deleteGlycanById(Long id) {
-		//		find an employee by id
+		//		find an glycan by id
 		Glycan gly = glycanRepository.getById(id);
 
-		//		if there is not an employee who has the id, throw the error.
+		//		if there is not an glycan who has the id, throw the error.
 		if (gly == null) {
 			throw new RuntimeException("Glycan not found");
 		}
 
-        //		delete an employee who has the id from the database 
+        //		delete an glycan who has the id from the database 
         glycanRepository.deleteById(id);
         return "Deleted: " + gly.getPhotosImagePath() + " " + gly.getOxford() + " " + gly.getDatabaseID()+ " " 
                 + gly.getNeutralMass()+ " " + gly.getObservedMZ()+ " " + gly.getCS_M_Plus_2H_Charge_2()+ " " 
