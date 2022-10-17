65186c1497d6ce970174d595d1386299f241e5ca
Aryeh <83979895+aryehjc@users.noreply.github.com>
2022-10-17 18:53:10 +0800

    Add files via upload

---
+++ src/main/java/com/example/demo/service/UserService.java
@@ -0,0 +1,27 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.example.demo.service;
+
+import com.example.demo.domain.Glycan;
+import com.example.demo.repository.GlycanRepository;
+import java.util.List;
+import javax.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.validation.annotation.Validated;
+
+/**
+ *
+ * @author aryeh
+ */
+@Service
+@Validated
+@RequiredArgsConstructor
+public class UserService {
+    public final GlycanRepository repoupload;
+    public void saveAll(@Valid List<Glycan> users) {
+        repoupload.saveAll(users);
+    }
+}
