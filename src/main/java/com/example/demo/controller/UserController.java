e94dcbb1a74dc684c3a21cbce575d3b03c91cbce
Aryeh <83979895+aryehjc@users.noreply.github.com>
2022-10-18 20:25:48 +0800

    Update UserController.java

--- src/main/java/com/example/demo/controller/UserController.java
+++ src/main/java/com/example/demo/controller/UserController.java
@@ -9,11 +9,15 @@ import com.example.demo.repository.GlycanRepository;
 import java.io.IOException;
 import java.io.InputStream;
 import lombok.RequiredArgsConstructor;
+import org.springframework.ui.Model;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.servlet.ModelAndView;
 
 /**
  *
@@ -28,12 +32,25 @@ public class UserController {
     
 
     @PostMapping(value = "/upload", consumes = "text/csv")
-    public void uploadSimple(@RequestBody InputStream body) throws IOException {
+    public ModelAndView uploadSimple(@RequestBody InputStream body, ModelMap model, Object glycanCSV) throws IOException {
         repoupload.saveAll(CsvUtils.read(Glycan.class, body));
+        
+        		model.addAttribute("glycan", glycanCSV);
+
+		//	shows the update_glycan.html template:
+		return new ModelAndView("redirect:/MySQLTableForGlycansAndCCS", model);
     }
 
     @PostMapping(value = "/upload", consumes = "multipart/form-data")
-    public void uploadMultipart(@RequestParam("file") MultipartFile file) throws IOException {
+    public ModelAndView uploadMultipart(@RequestParam("file") MultipartFile file, ModelMap model, Object glycanCSV) throws IOException {
         repoupload.saveAll(CsvUtils.read(Glycan.class, file.getInputStream()));
+        
+                		model.addAttribute("glycan", glycanCSV);
+
+		//	shows the update_glycan.html template:
+		return new ModelAndView("redirect:/MySQLTableForGlycansAndCCS", model);
+        
     }
+   
+
 }
