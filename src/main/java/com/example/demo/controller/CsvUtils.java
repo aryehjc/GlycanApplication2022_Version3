6b5619834a9d2a26daccd6b6ddaddd20b9454ecd
Aryeh <83979895+aryehjc@users.noreply.github.com>
2022-10-20 13:57:59 +0800

    Update CsvUtils.java

--- src/main/java/com/example/demo/controller/CsvUtils.java
+++ src/main/java/com/example/demo/controller/CsvUtils.java
@@ -6,6 +6,7 @@ package com.example.demo.controller;
 
 import com.fasterxml.jackson.databind.ObjectReader;
 import com.fasterxml.jackson.dataformat.csv.CsvMapper;
+import com.fasterxml.jackson.dataformat.csv.CsvParser.Feature;
 import com.fasterxml.jackson.dataformat.csv.CsvSchema;
 import java.io.IOException;
 import java.io.InputStream;
@@ -16,7 +17,7 @@ import java.util.List;
  * @author aryeh
  */
 public class CsvUtils {
-    public static final CsvMapper mapper = new CsvMapper();
+    public static final CsvMapper mapper = (new CsvMapper()).configure(Feature.IGNORE_TRAILING_UNMAPPABLE, true);
     public static <T> List<T> read(Class<T> clazz, InputStream stream) throws IOException {
         CsvSchema schema = mapper.schemaFor(clazz).withHeader().withColumnReordering(true);
         ObjectReader reader = mapper.readerFor(clazz).with(schema);
