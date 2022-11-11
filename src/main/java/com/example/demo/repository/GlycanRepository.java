1b10d619cf4693c895ef01751af00c2eaf525fab
Aryeh <83979895+aryehjc@users.noreply.github.com>
2022-11-11 15:37:38 +0800

    Update GlycanRepository.java

--- src/main/java/com/example/demo/repository/GlycanRepository.java
+++ src/main/java/com/example/demo/repository/GlycanRepository.java
@@ -12,7 +12,7 @@ import org.springframework.data.jpa.repository.Query;
 @Repository
 public interface GlycanRepository extends JpaRepository<Glycan, Long> {
 	// This will be AUTO IMPLEMENTED by Spring into a Bean
-  @Query("SELECT g FROM Glycan g WHERE CONCAT(g.id, ' ', g.DatabaseID, ' ', g.Oxford, ' ', g.NeutralMass, ' ', "
-          + "g.ObservedMZ, ' ', g.CS_M_Plus_2H_Charge_2, ' ', g.ObservedMZ2, ' ', g.CCS_M_Plus_H_Plus_Na_Charge_2) LIKE %?1%")
+  @Query("SELECT g FROM Glycan g WHERE CONCAT(g.id, ' ', g.databaseID, ' ', g.oxford, ' ', g.neutralmass, ' ', "
+          + "g.observedmz, ' ', g.ccs1, ' ', g.observedmz2, ' ', g.ccs2) LIKE %?1%")
 public Page<Glycan> findAll(String keyword, Pageable pageable);  
 }
