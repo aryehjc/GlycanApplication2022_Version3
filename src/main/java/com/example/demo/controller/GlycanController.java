e7643a4715da1c31dbd3563717cf4cd26118c412
Aryeh <83979895+aryehjc@users.noreply.github.com>
2022-11-11 15:39:20 +0800

    Update GlycanController.java

--- src/main/java/com/example/demo/controller/GlycanController.java
+++ src/main/java/com/example/demo/controller/GlycanController.java
@@ -12,9 +12,7 @@ import com.example.demo.domain.Glycan;
 import com.example.demo.service.GlycanService;
 import java.util.List;
 import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
 import org.springframework.data.repository.query.Param;
-import org.springframework.data.web.SortDefault;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 
