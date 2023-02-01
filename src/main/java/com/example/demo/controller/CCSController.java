279c22286f986d12d3877e86ef7d3b8715948d9f
Aryeh <83979895+aryehjc@users.noreply.github.com>
2023-02-01 15:45:40 +0800

    Update CCSController.java

--- src/main/java/com/example/demo/controller/CCSController.java
+++ src/main/java/com/example/demo/controller/CCSController.java
@@ -22,9 +22,14 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 
 
-import java.io.BufferedReader;
-import java.io.FileReader;
+import java.io.*;
+import com.example.demo.utils.PythonUtils;
+import com.graphbuilder.math.func.PowFunction;
+import java.io.InputStreamReader;
+import javax.security.sasl.Sasl;
+import org.springframework.web.multipart.MultipartFile;
 
+import javax.servlet.http.HttpSession;
 /**
  *
  * @author aryeh
@@ -35,12 +40,64 @@ public class CCSController {
    // ResourceLoader loader;
     
     @RequestMapping("/GraphDistributions")
-    public String ShowCCSDistribution(Model model) throws IOException {
+    public String ShowCCSDistribution(Model model,@RequestParam("file") MultipartFile mfile,  @RequestParam("file2") MultipartFile mfile2, HttpSession session) throws Exception {
 
-        String js_str = "";            
+        String sesson_path = session.getServletContext().getRealPath("/");     // this is the location of the directory the file will be placed
+        String session_tmp_file = mfile.getOriginalFilename();       // this is the tmepory file name (i think)       
+        String session_tmp_file2 = mfile2.getOriginalFilename();
+            
+        
+        // create a random directory for this session
+        int min = 1; // Minimum value of range
+        int max = 100000; // Maximum value of range      
+        // Generate random int value from min to max
+        int random_int = (int)Math.floor(Math.random() * (max - min + 1) + min);
+
+        
+        String workdir = sesson_path + "sample_" + random_int + "/";
+        File theDir = new File(workdir);
+        if (!theDir.exists()) {
+            theDir.mkdirs();
+        }
+        else {
+            System.out.println("Report back to front end an error saying this directory already exists (unlikely event");           
+        }
+        
+        
+        System.out.println(sesson_path + "/" + session_tmp_file);
+        
+        
+        File file_to_webapp_dir = new File(workdir + "/" + session_tmp_file);
+        mfile.transferTo(file_to_webapp_dir);
+        File file_to_webapp_dir2 = new File(workdir + "/" + session_tmp_file2);
+        mfile2.transferTo(file_to_webapp_dir2);
+        
+        
+        
+        // 1. run the python script on tzhe file now knowing that it is located at sesson_path + "/" + session_tmp_file                        
+        
+        // these variables are hardcoded to be the location of the pythioon environment with scripts and files
+        String hardcoded_python_location = "/home/aryeh/Downloads/demo/plotdistributions/";        
+        String python_venv = hardcoded_python_location + "/py39/bin/python3.9";
+        String pythn_script = hardcoded_python_location + "/CCSDistribution.py";
+        String library_location = hardcoded_python_location + "/library_distributions/frags_available.csv";
+        ////////////////////////////////
+        
+        
+        // these are supplied by the app and the user
+        String usermass_to_search_for = "708.33";   
+        //////
+        
+        
+        
+        PythonUtils py  = new PythonUtils();
+        System.out.println("TRYING: " + pythn_script + " " + workdir + " " + usermass_to_search_for + " 1 0.2 " + library_location);
+        py.executePythonScript(python_venv, new File(pythn_script), " " + workdir + " " + usermass_to_search_for + " 1 0.2 " + library_location);
+        
         
         // read the js returned by the python code 
-        BufferedReader readjs = new BufferedReader(new FileReader("/home/aryeh/Downloads/plotdistributions/sample/js_frags.708.36.txt")); 
+        String js_str = "";       
+        BufferedReader readjs = new BufferedReader(new FileReader(workdir + "/js_frags."+usermass_to_search_for+".txt")); 
         String line = readjs.readLine();
         while(line!=null) {
 
@@ -52,7 +109,7 @@ public class CCSController {
          String div_str = "";
          
          // read the div returned by the python code
-         BufferedReader readdiv = new BufferedReader(new FileReader("/home/aryeh/Downloads/plotdistributions/sample/div_frags.708.36.txt"));
+         BufferedReader readdiv = new BufferedReader(new FileReader(workdir + "/div_frags."+usermass_to_search_for+".txt"));
          String line2 = readdiv.readLine();
          while(line2!=null) {
              
